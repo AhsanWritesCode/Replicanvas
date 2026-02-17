c75f005cdb3b6db1d1780f78104eacd7fbded18f
NourAjami <ajami.nour4@gmail.com>
2026-02-17 14:31:10 -0700

    - Added HTTP endpoint to grab the snapshot of the canvas - Created the 2D array canvas type - Created websocket connection to allow multiple users to connect to it - Added backend structure to README file - Cleaned up files

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/AhsanWritesCode/559-project/internal/canvas"
+	"github.com/AhsanWritesCode/559-project/internal/server"
+)
+
+// entry point for the application, initializes the canvas and the HTTP and Websocket handlers and starts the server
+func main() {
+	canvas := canvas.NewCanvas(canvas.DefaultCanvasWdith, canvas.DefaultCanvasHeight)
+
+	//Create the HTTP and Websocket handlers here
+	httpHandler := server.NewHttpHandler(canvas)
+	wsHandler := server.NewWSHandler(canvas)
+
+	// Handle the HTTP requests and WebSocket connections
+	http.HandleFunc("/snapshot", httpHandler.GetSnapshot)
+	http.HandleFunc("/ws", wsHandler.HandleWS)
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ internal/server/http.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/AhsanWritesCode/559-project/internal/canvas"
+)
+
+// This struct is used to handle the HTTP requests and responses for the canvas
+type HttpHandler struct {
+	canvas *canvas.Canvas
+}
+
+// This function is used to create a new HTTP handler for the canvas
+func NewHttpHandler(canvas *canvas.Canvas) *HttpHandler {
+	return &HttpHandler{canvas: canvas}
+}
+
+// This struct is used to represent the response for the snapshot of the canvas (it will be sent to the client to be displayed on the canvas)
+type SnapShotResponse struct {
+	Width  int        `json:"width"`
+	Height int        `json:"height"`
+	Pixels [][]string `json:"pixels"`
+}
+
+// This request is used to get a snapshot of the canvas (it will be used when a client first connects to the server, or when a node crashes and reconnects to the server and wants to get the latest snapshot of the canvas)
+// This is not currently checking for any errors or validation of the request, in fact its not even checking if the request is a GET request (we are assuming its a GET request for now)
+// The frontend that is calling this will use GET request to get the snapshot of the canvas for now (we'll add validation and error checking later)
+func (h *HttpHandler) GetSnapshot(w http.ResponseWriter, r *http.Request) {
+
+	//content type and access control allow origin headers to allow the client to access the snapshot of the canvas
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	response := SnapShotResponse{
+		Width:  h.canvas.Width,
+		Height: h.canvas.Height,
+		Pixels: h.canvas.Snapshot(),
+	}
+	json.NewEncoder(w).Encode(response)
+}
